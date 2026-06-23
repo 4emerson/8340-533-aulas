b24c3cff8837bb86ee3539bed7ac162edf5c36a2
4emerson <emerson.silva@4linux.com.br>
2026-06-23 12:54:45 -0300

    ajustado LB

--- aulas/cap11/main.tf
+++ aulas/cap11/main.tf
@@ -1,6 +1,5 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  
 
   name = "modulo-publico-cap11"
   cidr = "10.0.0.0/16"
@@ -13,24 +12,29 @@ module "vpc" {
   enable_vpn_gateway = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
 
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
 
-  name = "single-instance"
-  ami =  "ami-0e68dc81dc36750a1"
+  name   = "ssh-cap11"
+  vpc_id = module.vpc.vpc_id
 
-  instance_type = "t3.small"
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
 
-  key_name      = "curso-533-key"
-  monitoring    = false
-  subnet_id     = module.vpc.public_subnets[0]
-  associate_public_ip_address = true
-  vpc_security_group_ids = [module.security_group.id]
+  egress_rules = ["all-all"]
 
   tags = {
     Terraform   = "true"
@@ -38,41 +42,90 @@ module "ec2_instance" {
   }
 }
 
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
 
-module "security_group" {
-  source = "terraform-aws-modules/security-group/aws"
-
-
-  name        = "cap11"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_rules = {
-    https = {
-      from_port   = 22
-      ip_protocol = "tcp"
-      cidr_ipv4   = "0.0.0.0/0"
-      
-    }
-  }
-
-  egress_rules = {
-    all = {
-      ip_protocol = "-1"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-  }
+  name                        = "single-instance"
+  ami                         = "ami-0e68dc81dc36750a1"
+  instance_type               = "t3.small"
+  key_name                    = "curso-533-key"
+  monitoring                  = false
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [module.security_group.security_group_id]
 
   tags = {
+    Terraform   = "true"
     Environment = "dev"
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "alb-cap11"
+  vpc_id  = module.vpc.vpc_id
+  subnets = [module.vpc.public_subnets[0], module.vpc.public_subnets[1]]
+
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    all_https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      description = "HTTPS web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "10.0.0.0/16"
+    }
+  }
+
+  listeners = {
+    ex-http = {
+      port     = 80
+      protocol = "HTTP"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix  = "h1"
+      protocol     = "HTTP"
+      port         = 80
+      target_type  = "instance"
+      target_id    = "i-0f1d462a2ccf78b50"
+    }
+  }
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
 
 output "azs" {
-    value = module.vpc.azs
-
+  value = module.vpc.azs
 }
 
 output "public_subnets" {
-    value = module.vpc.public_subnets
+  value = module.vpc.public_subnets
+}
+
+output "lb_dns_name" {
+  value = module.alb.dns_name
 }
