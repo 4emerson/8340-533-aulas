d4a0d37709fd0c39a2a905a87e8c01360a109e15
4emerson <emerson.silva@4linux.com.br>
2026-06-23 16:53:45 -0300

    count com operador tenario

--- aulas/cap12/count/exemplo-tenario/main.tf
+++ aulas/cap12/count/exemplo-tenario/main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "this" {
 
 resource "aws_subnet" "this" {
   vpc_id                  = aws_vpc.this.id
-  depends_on = [ aws_vpc.this ]
+  depends_on              = [aws_vpc.this]
   cidr_block              = "10.0.1.0/24"
   availability_zone       = terraform.workspace == "prd" ? "us-east-2a" : "us-east-2b"
   map_public_ip_on_launch = true
@@ -26,14 +26,14 @@ resource "aws_security_group" "this" {
   name   = format("%s-sg", terraform.workspace)
   vpc_id = aws_vpc.this.id
 
-  ingress  {
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress  {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -44,6 +44,7 @@ resource "aws_security_group" "this" {
 }
 
 resource "aws_instance" "this" {
+  count                  = terraform.workspace == "prd" ? 6 : 3
   ami                    = "ami-0e68dc81dc36750a1"
   instance_type          = terraform.workspace == "prd" ? "t3.micro" : "t3.small"
   availability_zone      = terraform.workspace == "prd" ? "us-east-2a" : "us-east-2b"
@@ -51,7 +52,7 @@ resource "aws_instance" "this" {
   vpc_security_group_ids = [aws_security_group.this.id]
 
   tags = {
-    Name        = format("%s-instancia", terraform.workspace)
+    Name        = format("%s-instancia-%d", terraform.workspace, count.index +1)
     Environment = terraform.workspace
   }
 
--- aulas/cap12/count/exemplo-tenario/output.tf
+++ aulas/cap12/count/exemplo-tenario/output.tf
@@ -1,14 +1,4 @@
-output "instancia_tipo" {
-  value = aws_instance.this.instance_type
-
-}
-
-output "instancia_az" {
-  value = aws_instance.this.availability_zone
-
-}
-
 output "instancia_ip" {
-  value = aws_instance.this.public_ip
+  value = aws_instance.this[*].public_ip
 
 }
