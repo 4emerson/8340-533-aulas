8e412439e2f0448639f30ea90757ebc119ff448e
4emerson <emerson.silva@4linux.com.br>
2026-06-25 14:59:57 -0300

    feat: criado vpc da infra final

---
+++ aulas/infra-final/variables.tf
@@ -0,0 +1,56 @@
+variable "region" {
+    type = string
+    default = "us-east-2"
+  
+}
+
+variable "vpc_cidr_block" {
+    type = string
+    default = "10.0.0.0/16"
+  
+}
+
+variable "subnet_cidr_block_a" {
+    type = string
+    default = "10.0.1.0/24"
+  
+}
+
+variable "subnet_cidr_block_b" {
+    type = string
+    default = "10.0.2.0/24"
+  
+}
+
+
+
+variable "aws_route_table_cidr_block" {
+    type = string
+    default = "0.0.0.0/0"
+  
+}
+
+variable "az_a" {
+    type = string
+    default = "us-east-2a"
+  
+}
+
+variable "az_b" {
+    type = string
+    default = "us-east-2b"
+  
+}
+
+
+variable "map_public_ip_on_launch" {
+    type = bool
+    default = true
+  
+}
+
+variable "nome" {
+    type = string
+    default = "533curso"
+  
+}
---
+++ aulas/infra-final/vpc.tf
@@ -0,0 +1,67 @@
+resource "aws_vpc" "this" {
+    cidr_block = var.vpc_cidr_block
+
+    tags = {
+      Name = format("%s-vpc-%s", var.nome, terraform.workspace)
+      Environment = terraform.workspace
+    }
+  
+}
+
+resource "aws_internet_gateway" "this" {
+    vpc_id = aws_vpc.this.id
+
+    tags = {
+      Name = format("%s-igw-%s", var.nome, terraform.workspace)
+      Environment = terraform.workspace
+    }
+}
+
+resource "aws_subnet" "public_a" {
+    vpc_id = aws_vpc.this.id
+    cidr_block = var.subnet_cidr_block_a
+    availability_zone = var.az_a
+    map_public_ip_on_launch = var.map_public_ip_on_launch
+
+    tags = {
+      Name = format("%s-subnet-a-%s", var.nome, terraform.workspace)
+      Environment = terraform.workspace
+    }      
+}
+
+resource "aws_subnet" "public_b" {
+    vpc_id = aws_vpc.this.id
+    cidr_block = var.subnet_cidr_block_b
+    availability_zone = var.az_a
+    map_public_ip_on_launch = var.map_public_ip_on_launch
+
+    tags = {
+      Name = format("%s-subnet-b-%s", var.nome, terraform.workspace)
+      Environment = terraform.workspace
+    }      
+}
+
+resource "aws_route_table" "this" {
+    vpc_id = aws_vpc.this.id
+
+    route  {
+        cidr_block = var.aws_route_table_cidr_block
+        gateway_id = aws_internet_gateway.this.id
+    }
+    tags = {
+      Name = format("%s-rt-%s", var.nome, terraform.workspace)
+      Environment = terraform.workspace
+    }    
+}
+
+resource "aws_route_table_association" "a" {
+    subnet_id = aws_subnet.public_a.id
+    route_table_id = aws_route_table.this.id
+  
+}
+
+resource "aws_route_table_association" "b" {
+    subnet_id = aws_subnet.public_b.id
+    route_table_id = aws_route_table.this.id
+  
+}
